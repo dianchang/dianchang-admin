07ece11b35c30ac19874cbe4a32fd28edf992b44
hustlzp <hustlzp@qq.com>
2015-07-29 19:48:53 +0800

    Update development_sample.py

--- config/development_sample.py
+++ config/development_sample.py
@@ -8,14 +8,14 @@ class DevelopmentConfig(Config):
 
     # SQLAlchemy config
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/dianchang-admin"
-    SQLALCHEMY_BINDS = {'dc': "mysql+pymysql://root:@localhost/dianchang"},
-    ROOT_TOPIC_ID = 1,
-    PRODUCT_TOPIC_ID = 2,
-    ORGANIZATION_TOPIC_ID = 3,
-    POSITION_TOPIC_ID = 4,
-    SKILL_TOPIC_ID = 5,
-    PEOPLE_TOPIC_ID = 6,
-    OTHER_TOPIC_ID = 7,
-    NC_TOPIC_ID = 8,
-    CDN_HOST = "http://xxx.qiniudn.com",
+    SQLALCHEMY_BINDS = {'dc': "mysql+pymysql://root:@localhost/dianchang"}
+    ROOT_TOPIC_ID = 1
+    PRODUCT_TOPIC_ID = 2
+    ORGANIZATION_TOPIC_ID = 3
+    POSITION_TOPIC_ID = 4
+    SKILL_TOPIC_ID = 5
+    PEOPLE_TOPIC_ID = 6
+    OTHER_TOPIC_ID = 7
+    NC_TOPIC_ID = 8
+    CDN_HOST = "http://xxx.qiniudn.com"
     DC_DOMAIN = "http://localhost:5000"
