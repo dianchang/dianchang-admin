633bb738c729b652f144fcd451ca64e94b038aff
hustlzp <hustlzp@qq.com>
2015-07-29 19:44:11 +0800

    Rm invitation codes.

--- config/development_sample.py
+++ config/development_sample.py
@@ -7,4 +7,15 @@ class DevelopmentConfig(Config):
     DEBUG = True
 
     # SQLAlchemy config
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:password@localhost/dianchang-admin"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost/dianchang-admin"
+    SQLALCHEMY_BINDS = {'dc': "mysql+pymysql://root:@localhost/dianchang"},
+    ROOT_TOPIC_ID = 1,
+    PRODUCT_TOPIC_ID = 2,
+    ORGANIZATION_TOPIC_ID = 3,
+    POSITION_TOPIC_ID = 4,
+    SKILL_TOPIC_ID = 5
+    PEOPLE_TOPIC_ID = 6,
+    OTHER_TOPIC_ID = 7,
+    NC_TOPIC_ID = 8,
+    CDN_HOST = "http://xxx.qiniudn.com",
+    DC_DOMAIN = "http://localhost:5000"
--- tests/test_invitation.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-from .suite import BaseSuite
-
-
-class TestInvitation(BaseSuite):
-    def test_action(self):
-        rv = self.client.get('/invitation/action')
-        assert rv.status_code == 200
